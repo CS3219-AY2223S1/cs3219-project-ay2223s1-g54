8ebee44f724078b109616ae0c8f9424fa33278a3
jeromewjj <jeromewongjj@gmail.com>
2022-09-07 20:36:37 +0800

    Add waiting and collaboration pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import SignupPage from './components/SignupPage';
+import MatchingPage from './components/MatchingPage';
+import WaitingPage from './components/WaitingPage';
 import {Box} from "@mui/material";
 
 function App() {
@@ -10,6 +12,14 @@ function App() {
                     <Routes>
                         <Route exact path="/" element={<Navigate replace to="/signup" />}></Route>
                         <Route path="/signup" element={<SignupPage/>}/>
+                    </Routes>
+                    <Routes>
+                        <Route exact path="/" element={<Navigate replace to="/matching" />}></Route>
+                        <Route path="/matching" element={<MatchingPage/>}/>
+                    </Routes>
+                    <Routes>
+                        <Route exact path="/" element={<Navigate replace to="/waiting" />}></Route>
+                        <Route path="/waiting" element={<WaitingPage/>}/>
                     </Routes>
                 </Router>
             </Box>
---
+++ frontend/src/components/WaitingPage.js
@@ -0,0 +1,65 @@
+import {
+    Box,
+    Button,
+    Typography
+} from "@mui/material";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+import { useNavigate } from 'react-router-dom';
+import { CloseSharpIcon } from '@mui/icons-material/CloseSharp';
+import { io } from 'socket.io-client';
+import 
+
+
+
+function WaitingPage() {
+    const socket = io("http://localhost:8001");
+
+    socket.emit('match-request', 'email', 'difficulty', new Date().getTime(), socket.io.engine.id);
+    socket.on('match-sucess', () => handleSuccessMatch())
+
+    const navigate = useNavigate();
+    const handleCancel = () => {
+
+        navigate("/matching")
+    }
+
+    const handleSuccessMatch = () => {
+        navigate("/collaboration")
+    }
+
+    const renderTime = ({ remainingTime }) => {
+        return (
+            <div className="timer">
+                <div className="text" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', }}>Remaining</div>
+                <div className="value" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', }}>{remainingTime}</div>
+                <div className="text" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', }}>seconds</div>
+            </div>
+        );
+    };
+
+    return (
+        <Box display={"flex"} flexDirection={"column"} width={"90%"}>
+            <Typography variant={"h6"} align={"center"} marginBottom={"2rem"}>Looking for Match</Typography>
+            <Box display={"flex"} flexDirection={"row"} justifyContent={"center"}>
+                <CountdownCircleTimer
+                    isPlaying
+                    duration={30}
+                    colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+                    colorsTime={[30, 20, 10, 0]}
+                    onComplete={() => handleCancel()}
+                >
+                    {renderTime}
+                </CountdownCircleTimer>
+            </Box>
+            <Box display={"flex"} flexDirection={"row"} justifyContent={"center"}>
+                <Button variant={"outlined"} onClick={() => handleCancel()} startIcon={<CloseSharpIcon />} >back</Button>
+            </Box>
+        </Box>
+
+    )
+
+
+
+}
+
+export default WaitingPage;
