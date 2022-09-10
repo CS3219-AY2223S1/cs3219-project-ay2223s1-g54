bf0df20509ea6155530cafb07bd7062cea30cb44
Deyi Tan <deyixtan@gmail.com>
2022-09-10 08:13:26 +0800

    Add basic collaborative code editor

--- frontend/src/components/CollaborationPage.js
+++ frontend/src/components/CollaborationPage.js
@@ -5,18 +5,48 @@ import {
 } from "@mui/material";
 import {useNavigate} from 'react-router-dom';
 import CloseSharpIcon from '@mui/icons-material/CloseSharp';
-
-
+import {useState} from "react";
+import {UnControlled as CodeMirror} from 'react-codemirror2';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/monokai.css';
+import 'codemirror/mode/javascript/javascript.js'
+import socket from '../socket.js'
 
 function CollaborationPage() {
+    const [code, setCode] = useState("")
     const navigate = useNavigate();
     const handleCancel = () => {
         navigate("/matching")
     }
 
+    const options = {
+        lineNumbers: true,
+        mode: 'javascript',
+        theme: 'monokai'
+    }
+
+    socket.get().on('code-event', ({newCode}) => {
+        updateCodeFromSockets(newCode);
+    });
+
+    const updateCodeFromSockets = (newCode) => {
+        setCode(newCode)
+    }
+
+    const updateCodeInState = (newCode) => {
+       setCode(newCode);
+       socket.get().emit('code-event1', { room_id: localStorage.getItem('room_id'), newCode });
+    };
+
     return ( 
         <Box display={"flex"} flexDirection={"column"} width={"90%"}>
-            <Typography variant={"h6"} align={"center"} marginBottom={"2rem"}>Collaboration Page - Work in Progress</Typography>
+            <div>
+                <CodeMirror 
+                    value={code} 
+                    onChange={(editor, data, value) => updateCodeInState(value) }
+                    options={options}
+                />
+            </div>
             <Box display={"flex"} flexDirection={"row"} justifyContent={"center"}>
                 <Button variant={"outlined"} onClick={()=>handleCancel()} startIcon={<CloseSharpIcon />} >back</Button>
             </Box>
