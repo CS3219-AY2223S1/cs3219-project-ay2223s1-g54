af9be96fa081d2324ba4bb736b2def28031066bc
Deyi Tan <deyixtan@gmail.com>
2022-09-12 18:28:32 +0800

    Fix process.env typo

--- user-service/index.js
+++ user-service/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-const PORT = proces.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
