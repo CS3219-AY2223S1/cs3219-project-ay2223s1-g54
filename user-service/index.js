71ac7db10ca9c89cfcecaf1267b4327298257750
Deyi Tan <deyixtan@gmail.com>
2022-09-13 08:30:54 +0800

    Refactor user-service and update jwt handling logic

--- user-service/index.js
+++ user-service/index.js
@@ -1,25 +1,20 @@
 import express from "express";
 import cors from "cors";
+import user_controller from "./controller/user-controller.js";
+import { PORT } from "./config.js";
 
 const app = express();
-const PORT = process.env.PORT || 8000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors()); // config cors so that front-end can use
 app.options("*", cors());
-import { createUser, loginUser } from "./controller/user-controller.js";
 
-const router = express.Router();
-
-// Controller will contain all the User-defined Routes
-router.get("/", (_, res) => res.send("Hello World from user-service"));
-router.post("/signup", createUser);
-router.post("/login", loginUser);
-
-app.use("/api/user", router).all((_, res) => {
+app.use("/api/user", user_controller).all((_, res) => {
   res.setHeader("content-type", "application/json");
   res.setHeader("Access-Control-Allow-Origin", "*");
 });
-
+app.use((err, req, res, next) => res.status(500).json({ error: err.message }));
 app.listen(PORT, () => console.log(`user-service listening on port ${PORT}`));
+
+export default app;
