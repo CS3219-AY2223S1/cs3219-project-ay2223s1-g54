f62016099c2d6b5edcab67918681272fd2083e1f
yl-ang <yongliangang@gmail.com>
2022-10-07 19:32:34 +0800

    Update landing page

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,5 +1,6 @@
-import { Container, Box, Button, Typography } from "@mui/material";
+import { Container, Box, Button, Typography, Grid } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import landingPageImage from "../landingpage.jpg";
 
 const LandingPage = () => {
   const navigate = useNavigate();
@@ -12,47 +13,65 @@ const LandingPage = () => {
   };
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center", // horizontal
-        justifyContent: "center", // vertical
-        height: "100vh",
-      }}
+    <Grid
+      container
+      spacing={0}
+      direction="row"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: "100vh", background: "#F1DDBF" }}
     >
-      <Typography variant="h1">PeerPrep</Typography>
-      <Typography
-        variant="caption"
-        align="center"
-        fontSize={"24px"}
-        sx={{ width: "35%", margin: "20px" }}
-      >
-        PeerPrep is a interview preparation platform and a peer matching system
-        where students can find peers to practice whiteboard-style interview
-        questions together.
-      </Typography>
-      <Box>
-        <Button
-          sx={{ marginRight: "5px" }}
-          variant="outlined"
-          onClick={handleSignUp}
+      <Grid item xs={6}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center", // horizontal
+            justifyContent: "center", // vertical
+            height: "100vh",
+          }}
         >
-          <Typography variant="button" display="block" gutterBottom>
-            Sign Up
+          <Typography variant="h1" fontWeight="bold" color="black">
+            PeerPrep
           </Typography>
-        </Button>
-        <Button
-          sx={{ marginLeft: "5px" }}
-          variant="contained"
-          onClick={handleLogin}
-        >
-          <Typography variant="button" display="block" gutterBottom>
-            Login
+          <Typography
+            variant="caption"
+            align="center"
+            fontSize={"24px"}
+            sx={{ width: "35%", margin: "20px" }}
+          >
+            PeerPrep is an interview preparation platform and a peer matching
+            system where students can find peers to practice whiteboard-style
+            interview questions together.
           </Typography>
-        </Button>
-      </Box>
-    </Box>
+          <Box>
+            <Button
+              sx={{ marginRight: "5px" }}
+              variant="outlined"
+              onClick={handleSignUp}
+              size="large"
+            >
+              <Typography variant="button" display="block" gutterBottom>
+                Sign Up
+              </Typography>
+            </Button>
+            <Button
+              sx={{ marginLeft: "5px" }}
+              variant="contained"
+              onClick={handleLogin}
+              size="large"
+            >
+              <Typography variant="button" display="block" gutterBottom>
+                Login
+              </Typography>
+            </Button>
+          </Box>
+        </Box>
+      </Grid>
+      <Grid item xs={6}>
+        <img src={landingPageImage} alt="horse" />
+      </Grid>
+    </Grid>
   );
 };
 
