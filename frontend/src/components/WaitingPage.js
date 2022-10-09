66481b51e16a156ed0c1461c3f4332b924e0fc57
yl-ang <yongliangang@gmail.com>
2022-10-10 00:24:53 +0800

    Improve waiting countdown ui

--- frontend/src/components/WaitingPage.js
+++ frontend/src/components/WaitingPage.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Grid, Typography } from "@mui/material";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import { useNavigate, useLocation } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
@@ -10,6 +10,7 @@ function WaitingPage() {
   const { userId, socket } = auth;
 
   useEffect(() => {
+    console.log(location.state.difficulty);
     socket.on("readyForCollab", (collabData) => {
       navigate("/collaboration", { state: { collabData } });
     });
@@ -67,27 +68,54 @@ function WaitingPage() {
   };
 
   return (
-    <Box display={"flex"} flexDirection={"column"} width={"90%"}>
-      <Typography variant={"h6"} align={"center"} marginBottom={"2rem"}>
-        Looking for Match
-      </Typography>
-      <Box display={"flex"} flexDirection={"row"} justifyContent={"center"}>
-        <CountdownCircleTimer
-          isPlaying
-          duration={30}
-          colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
-          colorsTime={[30, 20, 10, 0]}
-          onComplete={() => handleFailureMatch()}
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: "100vh", background: "#F1DDBF" }}
+    >
+      <Box
+        sx={{
+          width: 300,
+          height: 350,
+          borderRadius: 3,
+          background: "white",
+          boxShadow: "0 6px 6px hsl(0deg 0% 0% / 0.3)",
+        }}
+      >
+        <Typography
+          variant={"h6"}
+          align={"center"}
+          marginBottom={"2rem"}
+          marginTop={"1.5rem"}
         >
-          {renderTime}
-        </CountdownCircleTimer>
+          Looking for Match
+        </Typography>
+        <Box
+          display={"flex"}
+          flexDirection={"row"}
+          justifyContent={"center"}
+          marginBottom="1rem"
+        >
+          <CountdownCircleTimer
+            isPlaying
+            duration={30}
+            colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+            colorsTime={[30, 20, 10, 0]}
+            onComplete={() => handleFailureMatch()}
+          >
+            {renderTime}
+          </CountdownCircleTimer>
+        </Box>
+        <Box display={"flex"} flexDirection={"row"} justifyContent={"center"}>
+          <Button variant={"outlined"} onClick={() => handleCancel()}>
+            back
+          </Button>
+        </Box>
       </Box>
-      <Box display={"flex"} flexDirection={"row"} justifyContent={"center"}>
-        <Button variant={"outlined"} onClick={() => handleCancel()}>
-          back
-        </Button>
-      </Box>
-    </Box>
+    </Grid>
   );
 }
 
