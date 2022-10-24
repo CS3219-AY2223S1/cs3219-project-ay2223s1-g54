55194dcb5cb9dbe894b85abbc03f744323b14196
yl-ang <yongliangang@gmail.com>
2022-10-24 20:23:39 +0800

    Add email confirmation uri to service configs

--- backend/user-service/src/configs.js
+++ backend/user-service/src/configs.js
@@ -7,6 +7,8 @@ export const DB_CLOUD_URI = process.env.DB_CLOUD_URI;
 export const DB_LOCAL_URI = process.env.DB_LOCAL_URI;
 export const MONGOGB_URI = ENV == "PROD" ? DB_CLOUD_URI : DB_LOCAL_URI;
 
+export const EMAIL_CONFIRMATION_URI =
+  process.env.EMAIL_CONFIRMATION_URI || "http://localhost:8002/confirm";
 export const EMAIL_CONFIRMATION_SECRET = process.env.EMAIL_CONFIRMATION_SECRET;
 export const GMAIL_MAILER = process.env.GMAIL_MAILER;
 export const GMAIL_PASSWORD = process.env.GMAIL_PASSWORD;
--- backend/user-service/src/nodeMailerConfig.js
+++ backend/user-service/src/nodeMailerConfig.js
@@ -1,5 +1,9 @@
 import * as nodemailer from "nodemailer";
-import { GMAIL_MAILER, GMAIL_PASSWORD } from "./configs.js";
+import {
+  EMAIL_CONFIRMATION_URI,
+  GMAIL_MAILER,
+  GMAIL_PASSWORD,
+} from "./configs.js";
 
 const user = GMAIL_MAILER;
 const pass = GMAIL_PASSWORD;
@@ -16,12 +20,13 @@ export const sendConfirmationEmail = (name, email, confirmationCode) => {
   transport.sendMail({
     from: user,
     to: email,
-    subject: "Please confirm your account",
+    subject: "Please confirm your account for peerprep",
     html: `<h1>Email Confirmation</h1>
     <h2>Hello ${name}</h2>
     <p>Thank you for signing up with Peerprep. Please confirm your email by clicking on the following link</p>
     <!-- We need to change this to a dynamic link ... -->
-    <a href=http://localhost:8002/confirm/${confirmationCode}> Click here</a>
+    <a href=${EMAIL_CONFIRMATION_URI}/${confirmationCode}> Click here</a>
+    <p>Happy coding,<br>Peerprep Team 54</p>
     </div>`,
   });
 };
