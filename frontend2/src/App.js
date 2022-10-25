9f5509fc0bfe87d6cf26c31e58d78c3f5608cd31
Deyi Tan <deyixtan@gmail.com>
2022-10-26 03:17:36 +0800

    Add Charka UI

--- frontend2/src/App.js
+++ frontend2/src/App.js
@@ -1,8 +1,17 @@
-import { Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  return <Routes></Routes>;
+  return (
+    <ChakraProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
+  );
 }
 
 export default App;
