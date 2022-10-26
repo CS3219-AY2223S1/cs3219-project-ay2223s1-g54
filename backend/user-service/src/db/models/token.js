7bb44eab05b443f08e0ae2c98c7d58c26d762a4b
yl-ang <yongliangang@gmail.com>
2022-10-26 23:42:37 +0800

    Create token.js

---
+++ backend/user-service/src/db/models/token.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+let TokenSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "user",
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    immutable: true,
+    default: () => Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    default: () => Date.now(),
+  },
+});
+
+const TokenModel = mongoose.model("TokenModel", TokenSchema);
+export { TokenModel };
