2b515fb8b97d7780f93264145a7ceca77c412cff
jeromewjj <jeromewongjj@gmail.com>
2022-10-27 12:28:50 +0800

    Update communicationEvents.js

--- backend/communication-service/events/communicationEvents.js
+++ backend/communication-service/events/communicationEvents.js
@@ -2,14 +2,34 @@ const registerCommunicationHandlers = async (io, pubClient, subClient) => {
   const defaultParams = [io, pubClient, subClient];
 
   subClient.subscribe("sendMessage", async (data) => {
-    await communicationEvent(defaultParams, data);
+    await sendMessageEvent(defaultParams, data);
+  });
+
+  subClient.subscribe("sendCallerSignal", async (data) => {
+    await sendCallerSignalEvent(defaultParams, data);
+  });
+
+  subClient.subscribe("sendResponderSignal", async (data) => {
+    await sendResponderSignalEvent(defaultParams, data);
   });
 };
 
-const communicationEvent = async (defaultParams, data) => {
+const sendMessageEvent = async (defaultParams, data) => {
   const [io, pubClient, subClient] = defaultParams;
 
   await pubClient.publish("receiveMessage", data);
+};
+
+const sendCallerSignalEvent = async (defaultParams, data) => {
+  const [io, pubClient, subClient] = defaultParams;
+
+  await pubClient.publish("receiveCallerSignal", data);
+};
+
+const sendResponderSignalEvent = async (defaultParams, data) => {
+  const [io, pubClient, subClient] = defaultParams;
+
+  await pubClient.publish("receiveResponderSignal", data);
 };
 
 export { registerCommunicationHandlers };
