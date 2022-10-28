7938fc6b9cf7a062b3942473a84a3443960a40b5
Deyi Tan <deyixtan@gmail.com>
2022-10-29 03:34:50 +0800

    Update form layout

--- frontend2/src/components/layouts/FormLayout.js
+++ frontend2/src/components/layouts/FormLayout.js
@@ -1,19 +1,17 @@
-import { Box, Flex, Heading, Stack } from "@chakra-ui/react";
+import { Box, Heading, Stack } from "@chakra-ui/react";
 import BaseLayout from "./BaseLayout";
 
 const FormLayout = (props) => {
   return (
     <BaseLayout>
-      <Flex align="center" justify="center">
-        <Stack mt="150">
-          <Heading textAlign="center" color="#ffffff">
-            {props.title}
-          </Heading>
-          <Box p="8" bg="white" rounded="lg" boxShadow="lg">
-            {props.form}
-          </Box>
-        </Stack>
-      </Flex>
+      <Stack mt="150" spacing="5">
+        <Heading textAlign="center" color="#ffffff">
+          {props.title}
+        </Heading>
+        <Box p="8" bg="#ffffff" rounded="lg" boxShadow="lg">
+          {props.form}
+        </Box>
+      </Stack>
     </BaseLayout>
   );
 };
