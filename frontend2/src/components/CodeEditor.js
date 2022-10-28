cce882774f10c77e5d552900ec273c745329cd4b
Deyi Tan <deyixtan@gmail.com>
2022-10-29 07:54:12 +0800

    Add code editor theme selector

--- frontend2/src/components/CodeEditor.js
+++ frontend2/src/components/CodeEditor.js
@@ -5,8 +5,13 @@ const CodeEditor = () => {
   return (
     <Box w="full" minH="full" maxH="full" h="full">
       <Flex direction="column" minH="full" maxH="full" h="full">
-        <Flex pb="3" justify="flex-end">
-          <Select variant="filled">
+        <Flex pb="3">
+          <Select variant="filled" mr="2">
+            <option value="testTheme1">Theme 1</option>
+            <option value="testTheme2">Theme 2</option>
+            <option value="testTheme3">Theme 3</option>
+          </Select>
+          <Select variant="filled" ml="2">
             <option value="testLanguage1">Language 1</option>
             <option value="testLanguage2">Language 2</option>
             <option value="testLanguage3">Language 3</option>
