26a238269a75429e32c5c985e800c267edf209c3
YuHeem <54734556+yuheem@users.noreply.github.com>
2022-10-28 22:57:18 +0800

    Update frontend to use local judge

--- backend/gateway/src/routes/gatewayRoutes.js
+++ backend/gateway/src/routes/gatewayRoutes.js
@@ -2,11 +2,13 @@ import express from "express";
 import { authRoutes } from "./authRoutes.js";
 import { userRoutes } from "./userRoutes.js";
 import { questionRoutes } from "./questionRoutes.js";
+import { judgeRoutes } from "./judgeRoutes.js";
 
 const gatewayRoutes = express.Router();
 
 gatewayRoutes.use("/auth", authRoutes);
 gatewayRoutes.use("/user", userRoutes);
 gatewayRoutes.use("/question", questionRoutes);
+gatewayRoutes.use("/judge", judgeRoutes);
 
 export { gatewayRoutes };
