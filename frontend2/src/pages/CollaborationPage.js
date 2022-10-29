a671dcf1c55ae2fa2e4cc9f54df5bde28e5ceba4
Deyi Tan <deyixtan@gmail.com>
2022-10-29 08:06:42 +0800

    Add judge tab, submit and leave room buttons

--- frontend2/src/pages/CollaborationPage.js
+++ frontend2/src/pages/CollaborationPage.js
@@ -1,6 +1,8 @@
 import {
   Box,
+  Button,
   Flex,
+  Stack,
   Tab,
   Tabs,
   TabList,
@@ -14,33 +16,44 @@ import BaseLayout from "../components/layouts/BaseLayout";
 const CollaborationPage = () => {
   return (
     <BaseLayout>
-      <Flex w="full" h="full" minH="full" maxH="full">
-        <Box w="50%" h="full" minH="full" maxH="full">
-          <QuestionPane />
-        </Box>
-        <Tabs
-          w="50%"
-          h="full"
-          minH="full"
-          maxH="full"
-          bg="#ffffff"
-          isFitted
-          variant="enclosed"
-          colorScheme="green"
-        >
-          <TabList>
-            <Tab>Code Editor</Tab>
-            <Tab>White Board</Tab>
-          </TabList>
-
-          <TabPanels h="full" minH="full" maxH="full">
-            <TabPanel h="full" minH="full" maxH="full">
-              <CodeEditor />
-            </TabPanel>
-            <TabPanel></TabPanel>
-          </TabPanels>
-        </Tabs>
-      </Flex>
+      <Stack w="full" h="full" minH="full" maxH="full">
+        <Flex pt="2" pr="2" justify="flex-end">
+          <Button mr="2" colorScheme="blue">
+            Submit
+          </Button>
+          <Button mr="2" colorScheme="red">
+            Leave Room
+          </Button>
+        </Flex>
+        <Flex w="full" h="full" minH="full" maxH="full">
+          <Box w="50%" h="full" minH="full" maxH="full">
+            <QuestionPane />
+          </Box>
+          <Tabs
+            w="50%"
+            h="full"
+            minH="full"
+            maxH="full"
+            bg="#ffffff"
+            isFitted
+            variant="enclosed"
+            colorScheme="green"
+          >
+            <TabList>
+              <Tab>Code Editor</Tab>
+              <Tab>White Board</Tab>
+              <Tab>Judge</Tab>
+            </TabList>
+            <TabPanels h="full" minH="full" maxH="full">
+              <TabPanel h="full" minH="full" maxH="full">
+                <CodeEditor />
+              </TabPanel>
+              <TabPanel></TabPanel>
+              <TabPanel></TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Flex>
+      </Stack>
     </BaseLayout>
   );
 };
