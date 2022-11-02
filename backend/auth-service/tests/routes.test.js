31c8b2473a741096b1897b47b218fd5296409e0f
yl-ang <yongliangang@gmail.com>
2022-11-02 20:58:29 +0800

    Add negative test cases for auth routes

--- backend/auth-service/tests/routes.test.js
+++ backend/auth-service/tests/routes.test.js
@@ -1,5 +1,10 @@
 import supertest from "supertest";
 import { app } from "../src/app.js";
+import {
+  MISSING_ACCESS_TOKEN_FIELD,
+  MISSING_REFRESH_TOKEN_FIELD,
+  MISSING_USER_ID_FIELD,
+} from "../src/constants/responseMessages.js";
 
 describe("Auth Endpoints", () => {
   let accessToken, refreshToken;
@@ -18,6 +23,16 @@ describe("Auth Endpoints", () => {
     });
   });
 
+  describe("Not Generate Tokens", () => {
+    it("Should not generate accessToken and refreshToken without userId", async () => {
+      const res = await supertest(app).post("/generate").send({});
+
+      expect(res.statusCode).toBe(400);
+      expect(res.body.error.name).toBe("MalformedRequest");
+      expect(res.body.error.message).toBe(MISSING_USER_ID_FIELD);
+    });
+  });
+
   describe("Verify accessToken", () => {
     it("Should successfully verify accessToken", async () => {
       const res = await supertest(app)
@@ -25,6 +40,16 @@ describe("Auth Endpoints", () => {
         .send({ accessToken, userId });
 
       expect(res.statusCode).toBe(200);
+    });
+  });
+
+  describe("Not Verify accessToken", () => {
+    it("Should not verify accessToken without accessToken", async () => {
+      const res = await supertest(app).post("/verify").send({ userId });
+
+      expect(res.statusCode).toBe(400);
+      expect(res.body.error.name).toBe("MalformedRequest");
+      expect(res.body.error.message).toBe(MISSING_ACCESS_TOKEN_FIELD);
     });
   });
 
@@ -37,11 +62,31 @@ describe("Auth Endpoints", () => {
     });
   });
 
+  describe("Not Renew accessToken", () => {
+    it("Should not renew accessToken without refreshToken", async () => {
+      const res = await supertest(app).post("/renew").send({});
+
+      expect(res.statusCode).toBe(400);
+      expect(res.body.error.name).toBe("MalformedRequest");
+      expect(res.body.error.message).toBe(MISSING_REFRESH_TOKEN_FIELD);
+    });
+  });
+
   describe("Revoke refreshToken", () => {
     it("Should successfully revoke refreshToken", async () => {
       const res = await supertest(app).post("/revoke").send({ refreshToken });
 
       expect(res.statusCode).toBe(200);
+    });
+  });
+
+  describe("Not Revoke refreshToken", () => {
+    it("Should not revoke refreshToken without refreshToken", async () => {
+      const res = await supertest(app).post("/revoke").send({});
+
+      expect(res.statusCode).toBe(400);
+      expect(res.body.error.name).toBe("MalformedRequest");
+      expect(res.body.error.message).toBe(MISSING_REFRESH_TOKEN_FIELD);
     });
   });
 });
