b24896122d36763e6d734d8d12a84ee113e591ac
yl-ang <yongliangang@gmail.com>
2022-11-02 14:45:53 +0800

    Add negative test cases for userService

--- backend/user-service/tests/services/userService.test.js
+++ backend/user-service/tests/services/userService.test.js
@@ -1,5 +1,12 @@
 import * as userService from "../../src/services/userService.js";
 import { dbInit, dbTerminate } from "../../src/db/setup.js";
+import {
+  EMAIL_VALIDATION_FAIL,
+  GET_USER_BY_CONFIRMATION_CODE_FAILURE,
+  PASSWORD_VALIDATION_FAIL,
+  USERNAME_VALIDATION_FAIL,
+  USER_NOT_FOUND,
+} from "../../src/constants/responseMessages.js";
 
 describe("User Service", () => {
   beforeAll(async () => {
@@ -15,6 +22,11 @@ describe("User Service", () => {
   const oldPassword = password;
   const newPassword = "jesttest1234";
   const resetPassword = "jesttest5678";
+
+  const invalidEmail = "test@test";
+  const invalidUsernameStartWithNumber = "1jesttest123";
+  const invalidShortPassword = "12345";
+  const invalidConfirmationCode = "123456";
 
   describe("createUser", () => {
     it("Should create user successfully", async () => {
@@ -117,5 +129,57 @@ describe("User Service", () => {
 
   afterAll(async () => {
     await dbTerminate();
+  });
+
+  describe("createUserFail", () => {
+    it("Should not create user with invalid email", async () => {
+      const f = async () => {
+        const createdUser = await userService.createUser(
+          invalidEmail,
+          username,
+          password
+        );
+      };
+
+      await expect(f()).rejects.toThrow(EMAIL_VALIDATION_FAIL);
+    });
+  });
+
+  describe("createUserFail", () => {
+    it("Should not create user with invalid username starting with number", async () => {
+      const f = async () => {
+        const createdUser = await userService.createUser(
+          email,
+          invalidUsernameStartWithNumber,
+          password
+        );
+      };
+
+      await expect(f()).rejects.toThrow(USERNAME_VALIDATION_FAIL);
+    });
+  });
+
+  describe("createUserFail", () => {
+    it("Should not create user with invalid short password", async () => {
+      const f = async () => {
+        const createdUser = await userService.createUser(
+          email,
+          username,
+          invalidShortPassword
+        );
+      };
+
+      await expect(f()).rejects.toThrow(PASSWORD_VALIDATION_FAIL);
+    });
+  });
+
+  describe("confirmUserFail", () => {
+    it("Should not confirm account with incorrect confirmation code", async () => {
+      const f = async () => {
+        await userService.emailVerifyingUser(invalidConfirmationCode);
+      };
+
+      await expect(f()).rejects.toThrow(USER_NOT_FOUND);
+    });
   });
 });
