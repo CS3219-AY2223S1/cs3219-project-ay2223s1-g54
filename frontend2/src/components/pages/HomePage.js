9a92ba8c2acd25815d99e29a8d7d7d95dad26a56
Deyi Tan <deyixtan@gmail.com>
2022-11-07 07:19:40 +0800

    Add basic Navbar to HomePage

--- frontend2/src/components/pages/HomePage.js
+++ frontend2/src/components/pages/HomePage.js
@@ -1,11 +1,50 @@
+import {
+  Box,
+  Button,
+  Container,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
 import MatchForm from "../MatchForm";
-import BasicLayout from "../layouts/BasicLayout";
+import Navbar from "../Navbar";
+import BaseLayout from "../layouts/BaseLayout";
+import useAuth from "../../hooks/useAuth";
 
 const HomePage = () => {
+  const { auth } = useAuth();
+  const { username } = auth;
+
   return (
-    <BasicLayout>
-      <MatchForm />
-    </BasicLayout>
+    <BaseLayout>
+      <Navbar>
+        <Menu>
+          <MenuButton
+            minW="0"
+            variant="link"
+            rounded="full"
+            cursor="pointer"
+            as={Button}
+          >
+            {username}
+          </MenuButton>
+          <MenuList alignItems="center">
+            <MenuItem>Item1</MenuItem>
+            <MenuItem>Item2</MenuItem>
+            <MenuDivider />
+            <MenuItem>Item3</MenuItem>
+          </MenuList>
+        </Menu>
+      </Navbar>
+
+      <Container pt="100">
+        <Box p="8" bg="#ffffff" rounded="lg" boxShadow="lg">
+          <MatchForm />
+        </Box>
+      </Container>
+    </BaseLayout>
   );
 };
 
