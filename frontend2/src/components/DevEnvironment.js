74b63b3d7919dfa382d8159d13677dd42dbf7259
Deyi Tan <deyixtan@gmail.com>
2022-11-06 20:28:55 +0800

    Add code compilation logic

--- frontend2/src/components/DevEnvironment.js
+++ frontend2/src/components/DevEnvironment.js
@@ -13,13 +13,20 @@ import {
   Stack,
   Textarea,
 } from "@chakra-ui/react";
+import { URL_JUDGE_SVC_SUBMISSION } from "../configs";
 import CodeEditor from "../components/CodeEditor";
+import useAxiosPrivate from "../hooks/useAxiosPrivate";
 
 const DevEnvironment = (props) => {
   const [codeEditorTheme, setCodeEditorTheme] = useState("vs-dark");
   const [languageMap, setLanguageMap] = useState({});
   const [languageSlug, setLanguageSlug] = useState("");
+  const [languageId, setLanguageId] = useState(null);
+  const [programInput, setProgramInput] = useState("");
+  const [programOutput, setProgramOutput] = useState("");
+  const [isCompiling, setIsCompiling] = useState(false);
   const [code, setCode] = useState("");
+  const axiosPrivate = useAxiosPrivate();
   const { socket, roomId, codeSnippets } = props;
 
   const languageIdMap = new Map();
@@ -45,8 +52,7 @@ const DevEnvironment = (props) => {
     socket.on("receiveLanguage", ({ language }) => {
       updateCode(languageMap[language].code);
       setLanguageSlug(language);
-      // TODO: delete?
-      // props.onSetLanguageId(languageIdMap.get(language));
+      setLanguageId(languageIdMap.get(language));
     });
 
     socket.on("receiveCurrentCode", ({ code }) => {
@@ -82,8 +88,7 @@ const DevEnvironment = (props) => {
       const firstSlug = Object.keys(languageMap)[0];
       setLanguageMap(languageMap);
       setLanguageSlug(firstSlug);
-      // TODO: delete?
-      // props.onSetLanguageId(languageIdMap.get(firstSlug));
+      setLanguageId(languageIdMap.get(firstSlug));
       setCode(languageMap[firstSlug].code);
     }
   };
@@ -96,6 +101,86 @@ const DevEnvironment = (props) => {
   const updateLanguage = (event) => {
     const language = event.target.value;
     socket.emit("sendLanguage", { roomId, language });
+  };
+
+  const handleProgramInput = (event) => {
+    setProgramInput(event.target.value);
+  };
+
+  const updateProgramOutput = (compilationResult) => {
+    const statusId = compilationResult?.status?.id;
+
+    if (statusId === 3) {
+      setProgramOutput(compilationResult?.stdout || "No output (status: 3)");
+    } else if (statusId === 5) {
+      setProgramOutput("Time Limit Exceeded" || "No output (status: 5)");
+    } else if (statusId === 6) {
+      // compilation error
+      setProgramOutput(
+        compilationResult?.compile_output || "No output (status: 6)"
+      );
+    } else {
+      setProgramOutput(compilationResult?.stderr || "No output (all)");
+    }
+    setIsCompiling(false);
+  };
+
+  const handleCodeCompilation = async () => {
+    setIsCompiling(true);
+
+    const payload = {
+      language_id: languageId,
+      source_code: code,
+      stdin: programInput,
+    };
+
+    const requestData = {
+      method: "POST",
+      url: URL_JUDGE_SVC_SUBMISSION,
+      params: { base64_encoded: "false", fields: "*" },
+      headers: { "Content-Type": "application/json" },
+      data: payload,
+    };
+
+    try {
+      const response = await axiosPrivate.request(requestData);
+      const token = response.data.token;
+      getCompilationResults(token);
+    } catch (err) {
+      let error = err.response ? err.response.data : err;
+      // get error status
+      let status = err.response.status;
+      console.log("status", status);
+      if (status === 429) {
+        console.log("too many requests", status);
+      }
+      console.log("catch block...", error);
+    }
+
+    // // swap
+    // // socket.emit("sendSubmitCode", { roomId });
+    // setIsCompiling(false);
+  };
+
+  const getCompilationResults = async (token) => {
+    const requestData = {
+      method: "GET",
+      url: `${URL_JUDGE_SVC_SUBMISSION}/${token}`,
+      params: { base64_encoded: "false", fields: "*" },
+    };
+
+    try {
+      const response = await axiosPrivate.request(requestData);
+      const statusId = response.data.status?.id;
+
+      if (statusId === 1 || statusId === 2) {
+        setTimeout(() => getCompilationResults(token), 2000);
+        return;
+      }
+      updateProgramOutput(response.data);
+    } catch (err) {
+      console.log("err", err);
+    }
   };
 
   const updateCode = (code) => {
@@ -144,18 +229,30 @@ const DevEnvironment = (props) => {
           <AccordionItem>
             <AccordionButton>
               <Box flex="1" textAlign="left">
-                Run Test
+                Code Compilation
               </Box>
               <AccordionIcon />
             </AccordionButton>
             <AccordionPanel>
               <Stack>
-                <Input size="md" placeholder="Specify standard input" />
+                <Input
+                  size="md"
+                  placeholder="Specify standard input"
+                  onChange={handleProgramInput}
+                />
                 <Textarea
                   placeholder="Program output will be shown here"
+                  value={programOutput}
                   isReadOnly
                 />
-                <Button colorScheme="green">Compile Code</Button>
+                <Button
+                  colorScheme="green"
+                  isLoading={isCompiling}
+                  loadingText="Compiling"
+                  onClick={handleCodeCompilation}
+                >
+                  Compile Code
+                </Button>
               </Stack>
             </AccordionPanel>
           </AccordionItem>
