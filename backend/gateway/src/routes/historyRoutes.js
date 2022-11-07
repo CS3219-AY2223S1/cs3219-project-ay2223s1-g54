c72102d8197a67f9181435f6e909a01f9be6e93d
YuHeem <54734556+yuheem@users.noreply.github.com>
2022-11-08 02:46:07 +0800

    Update gateway with history service

---
+++ backend/gateway/src/routes/historyRoutes.js
@@ -0,0 +1,32 @@
+import express from "express";
+import asyncHandler from "express-async-handler";
+import * as statusCodes from "../constants/statusCodes.js";
+import { verifyAccessToken } from "../middlewares/verifyAccessToken.js";
+import { createSubmission, getSubmissionHistory } from "../services/historyService.js";
+
+const historyRoutes = express.Router();
+
+historyRoutes.post(
+  "/submissions/:userId/:questionId",
+  asyncHandler(verifyAccessToken),
+  asyncHandler(async (req, res) => {
+    const userId = req.params.userId;
+    const questionId = req.params.questionId;
+    await createSubmission(userId, questionId, req.body);
+    res.status(statusCodes.OK);
+  })
+)
+
+historyRoutes.get(
+  "/submissions/:userId/:questionId/:number",
+  asyncHandler(verifyAccessToken),
+  asyncHandler(async (req, res) => {
+    const userId = req.params.userId;
+    const questionId = req.params.questionId;
+    const number = req.params.number;
+    const submissionHistory = await getSubmissionHistory(userId, questionId, number);
+    res.status(statusCodes.OK).json(submissionHistory);
+  })
+)
+
+export { historyRoutes };
