cf2b2497d2a852acbfb49c228389f429c90f9abd
Deyi Tan <deyixtan@gmail.com>
2022-11-08 22:34:45 +0800

    Add access token verification middleware on history routes in gateway

--- backend/gateway/src/routes/historyRoutes.js
+++ backend/gateway/src/routes/historyRoutes.js
@@ -2,40 +2,48 @@ import express from "express";
 import asyncHandler from "express-async-handler";
 import * as statusCodes from "../constants/statusCodes.js";
 import { verifyAccessToken } from "../middlewares/verifyAccessToken.js";
-import { createSubmission, getSubmissionHistory, getUserSubmissionHistory } from "../services/historyService.js";
+import {
+  createSubmission,
+  getSubmissionHistory,
+  getUserSubmissionHistory,
+} from "../services/historyService.js";
 const historyRoutes = express.Router();
 
 historyRoutes.post(
   "/submissions/:userId/:questionId",
-  // asyncHandler(verifyAccessToken),
+  asyncHandler(verifyAccessToken),
   asyncHandler(async (req, res) => {
     const userId = req.params.userId;
     const questionId = req.params.questionId;
     await createSubmission(userId, questionId, req.body);
     res.status(statusCodes.OK);
   })
-)
+);
 
 historyRoutes.get(
   "/submissions/:userId/:questionId/:number",
-  // asyncHandler(verifyAccessToken),
+  asyncHandler(verifyAccessToken),
   asyncHandler(async (req, res) => {
     const userId = req.params.userId;
     const questionId = req.params.questionId;
     const number = req.params.number;
-    const submissionHistory = await getSubmissionHistory(userId, questionId, number);
+    const submissionHistory = await getSubmissionHistory(
+      userId,
+      questionId,
+      number
+    );
     res.status(statusCodes.OK).json(submissionHistory);
   })
-)
+);
 
 historyRoutes.get(
   "/submissions/:userId",
-  // asyncHandler(verifyAccessToken),
+  asyncHandler(verifyAccessToken),
   asyncHandler(async (req, res) => {
     const userId = req.params.userId;
     const userSubmissionHistory = await getUserSubmissionHistory(userId);
     res.status(statusCodes.OK).json(userSubmissionHistory);
   })
-)
+);
 
 export { historyRoutes };
